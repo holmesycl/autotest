25eb0be97fb511252a9458eb8326475321f050e1
holmesycl <1047406364@qq.com>
2016-11-15 01:02:53 +0800

    Signed-off-by: holmesycl <1047406364@qq.com>

---
+++ src/main/java/com/asiainfo/sh/rule/RuleAutotestApplication.java
@@ -0,0 +1,12 @@
+package com.asiainfo.sh.rule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleAutotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleAutotestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/asiainfo/sh/rule/RuleAutotestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.asiainfo.sh.rule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RuleAutotestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
